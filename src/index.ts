65cc58a0667d952518261b2569d73d2dc6a23257
thanhdt-viosoft <thanhdt@viosoft.com>
2017-07-05 11:35:27 +0700

    Remove unused library

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,12 @@ import * as path from 'path'
 import { Server } from 'hinos'
 import { route } from 'hinos-route'
 import { Mongo } from 'hinos-mongo'
-import { serve } from 'hinos-serve'
 import { cors } from 'hinos-cors'
 import './config'
 
 require(`./env.${Server.env}`).default(Server)
 
 Mongo(AppConfig.mongo)
-Server.use(serve({
-  '/images': path.join(__dirname, '..', 'assets', 'images')
-}))
 Server.use(cors())
 Server.use(route(path.join(__dirname, 'controller')))
 
