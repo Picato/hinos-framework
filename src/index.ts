62d93d41a5afd515d8af12e67062d0990bc4d193
thanhdt-viosoft <thanhdt@viosoft.com>
2017-07-05 11:09:51 +0700

    Change config to package.json

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ Server.use(route(path.join(__dirname, 'controller')))
 
 Server.listen(AppConfig.port, () => {
   console.info(`
-       _     _                 
-      | |__ (_)_ __   ___  ___  ${AppConfig.port} 
-      | '_ \\| | '_ \\ / _ \\/ __|
-      | | | | | | | | (_) \\__ \\
-      |_| |_|_|_| |_|\\___/|___/
+    _     _                 
+  | |__ (_)_ __   ___  ___  ${AppConfig.name}
+  | '_ \\| | '_ \\ / _ \\/ __|
+  | | | | | | | | (_) \\__ \\ 
+  |_| |_|_|_| |_|\\___/|___/ ${AppConfig.port}
       
   `)
 })
