9bcd6f7bde4f8e3a4b751b5f7c82a2f9084a91f2
thanhdt-viosoft <thanhdt@viosoft.com>
2017-07-05 11:36:46 +0700

    Remove unused package

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as path from 'path'
 import { Server } from 'hinos'
 import { route } from 'hinos-route'
 import { Mongo } from 'hinos-mongo'
-import { serve } from 'hinos-serve'
 import { cors } from 'hinos-cors'
 import { MailService } from './service/MailService'
 import './config'
@@ -10,9 +9,6 @@ import './config'
 require(`./env.${Server.env}`).default(Server)
 
 Mongo(AppConfig.mongo)
-Server.use(serve({
-  '/images': path.join(__dirname, '..', 'assets', 'images')
-}))
 Server.use(cors())
 Server.use(route(path.join(__dirname, 'controller')))
 
