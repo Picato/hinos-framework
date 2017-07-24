40d50f6d3dcb781719535adf7d51fc4afe41025a
thanhdt-viosoft <thanhdt@viosoft.com>
2017-07-24 11:52:35 +0700

    Update oauth func

--- src/service/Authoriz.ts
+++ src/service/Authoriz.ts
@@ -3,7 +3,7 @@ import HttpError from '../common/HttpError'
 import { Mongo } from 'hinos-mongo'
 
 export function authoriz(pathCode: string, actions: string[]) {
-  return async (ctx: Context, next: Function) => {
+  return async ({ ctx, headers }: Context, next: Function) => {
     await next()
   }
 }
