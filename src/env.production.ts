065a03f9d0b9e63fdce8c82731fe45664a50fa81
thanhdt-viosoft <thanhdt@viosoft.com>
2017-08-16 14:37:41 +0700

    Integrate hash

--- src/env.production.ts
+++ src/env.production.ts
@@ -3,4 +3,8 @@ import { helmet } from 'hinos-helmet'
 export default function env(Server) {
   console.log('Production mode')
   Server.use(helmet.default())
+  if (AppConfig.encrypt && AppConfig.encrypt.pwd) {
+    const { hasher } = require('hinos-requesthasher')
+    Server.use(hasher(AppConfig.encrypt.pwd, AppConfig.encrypt))
+  }
 }
--- typings/globals/index.d.ts
+++ typings/globals/index.d.ts
@@ -1,27 +1,32 @@
 interface IAppConfig {
-    externalConfig?: string;
-    url: string;
-    name: string;
-    port?: number;
-    host?: string;
-    routes?: Array<any>
-    mongo?: {
-        url: string;
-        releaseTimeout?: number;
-    };
-    redis?: {
-        host: string;
-        port: number;
-        db?: string;
-        releaseTimeout?: number;
-    };
-    encryptKey?: string;
-    services: {
-        oauth: string
-    },
-    app: {
-        suid: string
-    }
+  externalConfig?: string
+  url: string
+  name: string
+  port?: number
+  host?: string
+  routes?: Array<any>
+  mongo?: {
+    url: string
+    releaseTimeout?: number
+  }
+  redis?: {
+    host: string
+    port: number
+    db?: string
+    releaseTimeout?: number
+  }
+  encrypt?: {
+    pwd?: string
+    mode?: Array<'query' | 'body' | 'response'>
+    type?: 'rsa' | 'blowfish'
+    baseOnRequest?: boolean
+  }
+  services: {
+    oauth: string
+  }
+  app: {
+    suid: string
+  }
 }
 
-declare const AppConfig: IAppConfig;
+declare const AppConfig: IAppConfig
