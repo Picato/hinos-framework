5a2ddcb6f2b9a020cdba9fbe8186722a592d9a0c
thanhdt-viosoft <thanhdt@viosoft.com>
2017-09-11 14:20:58 +0700

    Update libs

--- src/service/Authoriz.ts
+++ src/service/Authoriz.ts
@@ -5,30 +5,36 @@ import HttpError from '../common/HttpError'
 
 export function authoriz(path: string, actions: string[]) {
   return async ({ ctx, headers }: Context, next: Function) => {
-    const res = await axios.head(`${AppConfig.services.oauth}/Oauth/Authoriz?path=${path}&actions=${actions.join(',')}`, {
-      headers: {
-        token: headers.token
+    try {
+      const res = await axios.head(`${AppConfig.services.oauth}/Oauth/Authoriz?path=${path}&actions=${actions.join(',')}`, {
+        headers: {
+          token: headers.token
+        }
+      })
+      ctx.state.auth = {
+        token: res.headers.token,
+        projectId: Mongo.uuid(res.headers.project_id),
+        accountId: Mongo.uuid(res.headers.account_id)
       }
-    })
-    if (res.status !== 204) throw HttpError.CUSTOMIZE(res.status, res.data || res.statusText)
-    ctx.state.auth = {
-      token: res.headers.token,
-      projectId: Mongo.uuid(res.headers.project_id),
-      accountId: Mongo.uuid(res.headers.account_id)
+      await next()
+    } catch (e) {
+      throw HttpError.CUSTOMIZE(e.response.status, e.response.data)
     }
-    await next()
   }
 }
 
 export function suAuthoriz() {
   return async ({ headers }: Context, next: Function) => {
-    const res = await axios.head(`${AppConfig.services.oauth}/Oauth/Authoriz`, {
-      headers: {
-        token: headers.token
-      }
-    })
-    if (res.status !== 204) throw HttpError.INTERNAL(res.data || res.statusText)
-    if (Object.keys(res.headers).includes('token')) throw HttpError.AUTHORIZ('This api need super admin permission')
-    await next()
+    try {
+      const res = await axios.head(`${AppConfig.services.oauth}/Oauth/Authoriz`, {
+        headers: {
+          token: headers.token
+        }
+      })
+      if (Object.keys(res.headers).includes('token')) throw HttpError.AUTHORIZ('This api need super admin permission')
+      await next()
+    } catch (e) {
+      throw HttpError.CUSTOMIZE(e.response.status, e.response.data)
+    }
   }
 }
