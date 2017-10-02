6de6e04b63781d9b2f067abadc2c0e4b993afa67
thanhdt-viosoft <thanhdt@viosoft.com>
2017-10-02 12:02:24 +0700

    Add auto hash in dev mode

--- src/env.development.ts
+++ src/env.development.ts
@@ -1,3 +1,7 @@
-export default function env() {
+export default function env(Server) {
   console.log('Development mode')
+  if (AppConfig.encrypt && AppConfig.encrypt.pwd) {
+    const { hasher } = require('hinos-requesthasher')
+    Server.use(hasher(AppConfig.encrypt.pwd, AppConfig.encrypt))
+  }
 }
