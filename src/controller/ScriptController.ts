ceb10c9ed8853ef0ccb4f52b1c3c79f86f78386c
thanhdt-viosoft <thanhdt@viosoft.com>
2017-10-12 10:42:16 +0700

    Update AppConfig.path

--- src/controller/ScriptController.ts
+++ src/controller/ScriptController.ts
@@ -30,7 +30,7 @@ export class ScriptController {
   }
 
   @GET('/')
-  @INJECT(authoriz(`/script`, ['FIND']))
+  @INJECT(authoriz(`${AppConfig.path}`, ['FIND']))
   @RESTRICT({
     query: {
       page: Number,
@@ -72,7 +72,7 @@ export class ScriptController {
   }
 
   @GET('/:_id')
-  @INJECT(authoriz(`/script`, ['GET']))
+  @INJECT(authoriz(`${AppConfig.path}`, ['GET']))
   @RESTRICT({
     params: {
       _id: Mongo.uuid
@@ -84,7 +84,7 @@ export class ScriptController {
   }
 
   @POST('/')
-  @INJECT(authoriz(`/script`, ['INSERT']))
+  @INJECT(authoriz(`${AppConfig.path}`, ['INSERT']))
   @BODYPARSER()
   @RESTRICT({
     body: {
@@ -104,7 +104,7 @@ export class ScriptController {
   }
 
   @PUT('/:_id')
-  @INJECT(authoriz(`/script`, ['UPDATE']))
+  @INJECT(authoriz(`${AppConfig.path}`, ['UPDATE']))
   @BODYPARSER()
   @RESTRICT({
     params: {
@@ -129,7 +129,7 @@ export class ScriptController {
   }
 
   @DELETE('/:_id')
-  @INJECT(authoriz(`/script`, ['DELETE']))
+  @INJECT(authoriz(`${AppConfig.path}`, ['DELETE']))
   @RESTRICT({
     params: {
       _id: Mongo.uuid
