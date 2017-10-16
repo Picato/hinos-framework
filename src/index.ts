d48bf600801d0b1f8347b8b1523179c0f1561c88
thanhdt-viosoft <thanhdt@viosoft.com>
2017-10-16 15:54:51 +0700

    Remove serve for DOC

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as path from 'path'
 import { Server } from 'hinos'
-import { serve } from 'hinos-serve'
 import { route } from 'hinos-route'
 import { Mongo } from 'hinos-mongo'
 import { cors } from 'hinos-cors'
@@ -11,9 +10,6 @@ require(`./env.${Server.env}`).default(Server)
 
 Mongo(AppConfig.mongo).debug(!Server.isProduction)
 
-Server.use(serve({
-  [`${AppConfig.path}/assets`]: path.join(__dirname, '..', 'assets')
-}))
 Server.use(cors())
 Server.use(route(path.join(__dirname, 'controller'), {
   ignorecase: true,
