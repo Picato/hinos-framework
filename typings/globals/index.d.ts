c061da05a847c2df7938351a2661da81fc3cb209
thanhdt-viosoft <thanhdt@viosoft.com>
2017-11-16 11:12:35 +0700

    Optimize

--- typings/globals/index.d.ts
+++ typings/globals/index.d.ts
@@ -20,7 +20,7 @@ interface IAppConfig {
     pwd?: string
     mode?: Array<'query' | 'body' | 'response'>
     baseOnRequest?: boolean
-    helmet: boolean
+    helmet?: boolean
   }
   services: {
     oauth: string
