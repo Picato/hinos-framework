6938b57137df16ef757d0e02bbcaee45b692bf90
thanhdt-viosoft <thanhdt@viosoft.com>
2017-12-08 10:41:35 +0700

    Remove listener

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import * as path from 'path'
+// import * as path from 'path'
 import { Server } from 'hinos'
-import { route } from 'hinos-route'
+// import { route } from 'hinos-route'
 import { Mongo } from 'hinos-mongo'
 import { Redis } from 'hinos-redis'
-import { serve } from 'hinos-serve'
-import { cors } from 'hinos-cors'
+// import { serve } from 'hinos-serve'
+// import { cors } from 'hinos-cors'
 import './config'
 import { GoldService } from './service/GoldService';
 import { CoinService } from './service/CoinService';
@@ -13,21 +13,22 @@ require(`./env.${Server.env}`).default(Server)
 
 Mongo(AppConfig.mongo).debug(!Server.isProduction)
 Redis(AppConfig.redis).debug(!Server.isProduction)
-Server.use(serve({
-  '/images': path.join(__dirname, '..', 'assets', 'images')
-}))
-Server.use(cors())
-Server.use(route(path.join(__dirname, 'controller'), { ignorecase: true, root: AppConfig.path }))
+// Server.use(serve({
+//   '/images': path.join(__dirname, '..', 'assets', 'images')
+// }))
+// Server.use(cors())
+// Server.use(route(path.join(__dirname, 'controller'), { ignorecase: true, root: AppConfig.path }))
 
-Server.listen(AppConfig.port, () => {
-  console.info(`
-    _     _
-  | |__ (_)_ __   ___  ___  ${AppConfig.name}
-  | '_ \\| | '_ \\ / _ \\/ __|
-  | | | | | | | | (_) \\__ \\
-  |_| |_|_|_| |_|\\___/|___/ ${AppConfig.port}
+// Server.listen(AppConfig.port, () => {
+//   console.info(`
+//     _     _
+//   | |__ (_)_ __   ___  ___  ${AppConfig.name}
+//   | '_ \\| | '_ \\ / _ \\/ __|
+//   | | | | | | | | (_) \\__ \\
+//   |_| |_|_|_| |_|\\___/|___/ ${AppConfig.port}
 
-  `)
-  GoldService.autoSync();
-  CoinService.autoSync();
-})
+//   `)  
+// })
+
+GoldService.autoSync();
+CoinService.autoSync();
