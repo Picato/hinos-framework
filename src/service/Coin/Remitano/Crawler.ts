b9298e7c18d668d04594d684a06123731601f5af
thanhdt-viosoft <thanhdt@viosoft.com>
2018-01-20 14:32:08 +0700

    Change to static

--- src/service/Coin/Remitano/Crawler.ts
+++ src/service/Coin/Remitano/Crawler.ts
@@ -2,8 +2,6 @@ import { MONGO, Mongo, Collection } from "hinos-mongo/lib/mongo";
 import HttpError from "../../../common/HttpError";
 import { REDIS, Redis } from "hinos-redis/lib/redis";
 
-const phantom = require('phantom');
-
 @Collection()
 export class RemitanoRate {
   currency: string
@@ -23,28 +21,30 @@ export class RemitanoRate {
   seconds: number
 }
 
-class RemitanoCrawler {
+export default class RemitanoCrawler {
 
   @MONGO('coin')
-  private mongo: Mongo
+  private static mongo: Mongo
 
   @REDIS()
-  private redis: Redis
+  private static redis: Redis
 
-  page
-  lastUpdateDB
+  private static phantom = require('phantom')
 
-  async getRate() {
-    const rs = await this.redis.get('remitano.rate')
+  static page
+  static lastUpdateDB
+
+  static async getRate() {
+    const rs = await RemitanoCrawler.redis.get('remitano.rate')
     return rs ? JSON.parse(rs) : {}
   }
 
-  async find(fil?) {
-    return await this.mongo.find<RemitanoRate>(RemitanoRate, fil)
+  static async find(fil?) {
+    return await RemitanoCrawler.mongo.find<RemitanoRate>(RemitanoRate, fil)
   }
 
-  async groupByTime(beforeThat: Date, groupBy: any) { //{ hours: '$hours', minutes: '$minutes' }
-    return await this.mongo.manual(RemitanoRate, async (collection) => {
+  static async groupByTime(beforeThat: Date, groupBy: any) { //{ hours: '$hours', minutes: '$minutes' }
+    return await RemitanoCrawler.mongo.manual(RemitanoRate, async (collection) => {
       const rs0 = collection.aggregate(
         [
           {
@@ -66,43 +66,38 @@ class RemitanoCrawler {
     })
   }
 
-  async init() {
-    const self = this
-    
-    setInterval(() => {
-      self.scan.apply(self)
-    }, AppConfig.app.bittrex.scanRemitano)
-
-    this.lastUpdateDB = await this.redis.manual(async redis => {
-      let lastUpdateDB = await this.redis._get(redis, `${this.constructor.name}.lastUpdateDB`)
+  static async init() {
+    RemitanoCrawler.lastUpdateDB = await RemitanoCrawler.redis.manual(async redis => {
+      let lastUpdateDB = await RemitanoCrawler.redis._get(redis, `${RemitanoCrawler.constructor.name}.lastUpdateDB`)
       lastUpdateDB = lastUpdateDB ? new Date(lastUpdateDB) : undefined
       return lastUpdateDB
     })
+    setInterval(RemitanoCrawler.scan, AppConfig.app.bittrex.scanRemitano)
   }
 
-  async scan() {
+  static async scan() {
     try {
       let status
-      if (!this.page) {
-        const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no', '--disk-cache=true'], {
+      if (!RemitanoCrawler.page) {
+        const instance = await RemitanoCrawler.phantom.create(['--ignore-ssl-errors=yes', '--load-images=no', '--disk-cache=true'], {
           // logger: yourCustomLogger,
           logLevel: 'error',
         });
-        this.page = await instance.createPage();
-        this.page.on('onResourceRequested', true, function (requestData, networkRequest) {
+        RemitanoCrawler.page = await instance.createPage();
+        RemitanoCrawler.page.on('onResourceRequested', true, function (requestData, networkRequest) {
           if (requestData.url.indexOf('remitano') === -1) return networkRequest.abort()
         })
-        status = await this.page.open('https://usdt.remitano.com/vn');
+        status = await RemitanoCrawler.page.open('https://usdt.remitano.com/vn');
         if (status !== 'success') throw HttpError.BAD_REQUEST('Could not request to remitano')
       } else {
-        await this.page.reload()
+        await RemitanoCrawler.page.reload()
       }
-      const content = await this.page.property('content')
+      const content = await RemitanoCrawler.page.property('content')
       const m = content.match(/"btc_rates":.*?"vn":(\{[^\}]+\})/)
       const rate = JSON.parse(m[1])
       rate.time = new Date()
-      if (!this.lastUpdateDB || (this.lastUpdateDB.getMinutes() !== rate.time.getMinutes() && rate.time.getMinutes() % 5 == 0)) {
-        this.lastUpdateDB = rate.time
+      if (!RemitanoCrawler.lastUpdateDB || (RemitanoCrawler.lastUpdateDB.getMinutes() !== rate.time.getMinutes() && rate.time.getMinutes() % 5 == 0)) {
+        RemitanoCrawler.lastUpdateDB = rate.time
         rate.day = rate.time.getDay()
         rate.date = rate.time.getDate()
         rate.month = rate.time.getMonth()
@@ -110,13 +105,12 @@ class RemitanoCrawler {
         rate.hours = rate.time.getHours()
         rate.minutes = rate.time.getMinutes()
         rate.seconds = rate.time.getSeconds()
-        await this.mongo.insert(RemitanoRate, rate)
-        await this.redis.set(`${this.constructor.name}.lastUpdateDB`, this.lastUpdateDB)
+        await RemitanoCrawler.mongo.insert(RemitanoRate, rate)
+        await RemitanoCrawler.redis.set(`${RemitanoCrawler.constructor.name}.lastUpdateDB`, RemitanoCrawler.lastUpdateDB)
       }
-      await this.redis.set('remitano.rate', JSON.stringify(rate))
+      await RemitanoCrawler.redis.set('remitano.rate', JSON.stringify(rate))
     } catch (e) {
       console.error('phantom:remitano', e)
     }
   }
 }
-export default new RemitanoCrawler()
