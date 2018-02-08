4c346ed295730be4417e3aaecf243f06ccfc123a
doanthuanthanh88 <doanthuanthanh88@gmail.com>
2018-02-08 10:14:19 +0700

    Update config

--- src/config.ts
+++ src/config.ts
@@ -5,17 +5,21 @@ declare let global: any
 
 const packageConfig = require('../package.json')
 let appconfig = packageConfig.appconfig.reduce((sum, e) => {
-  const [file, root] = e.split('?')
-  const c = require(`../${file}`)
-  if (root) {
-    const m = root.split(',').reduce((sum, e) => {
-      e = e.trim()
-      const m = e.startsWith('#') ? c[e.substr(1)] : { [e]: c[e] }
-      return _.merge(sum, m)
-    }, {})
-    sum = _.merge(sum, m)
-  } else {
-    sum = _.merge(sum, c)
+  try {
+    const [file, root] = e.split('?')
+    const c = require(`../${file}`)
+    if (root) {
+      const m = root.split(',').reduce((sum, e) => {
+        e = e.trim()
+        const m = e.startsWith('#') ? c[e.substr(1)] : { [e]: c[e] }
+        return _.merge(sum, m)
+      }, {})
+      sum = _.merge(sum, m)
+    } else {
+      sum = _.merge(sum, c)
+    }
+  } catch (e) {
+    console.error(e)
   }
   return sum
 }, {})
