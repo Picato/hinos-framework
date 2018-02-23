119080ffb18e4e710485104ead3a2d2c7e2dab64
thanhdt-viosoft <thanhdt@viosoft.com>
2018-02-23 10:04:36 +0700

    Update authoriz

--- src/service/Authoriz.ts
+++ src/service/Authoriz.ts
@@ -1,28 +1,24 @@
 import { Context } from 'hinos'
-import axios from 'axios'
 import { Mongo } from 'hinos-mongo'
+import axios from 'axios'
 import HttpError from '../common/HttpError'
 
 export function authoriz(path: string, action: string) {
   return async ({ ctx, headers }: Context, next: Function) => {
+    const token = headers.token
+    if (!token) throw HttpError.AUTHEN()
     try {
       const res = await axios.head(`${AppConfig.services.oauth}/oauth/Authoriz`, {
-        params: {
-          path,
-          action
-        },
-        headers: {
-          token: headers.token
-        }
+        params: { path, action },
+        headers: { token }
       })
       ctx.state.auth = {
-        token: headers.token,
+        token,
         projectId: Mongo.uuid(res.headers.project_id),
         accountId: Mongo.uuid(res.headers.account_id)
       }
       await next()
     } catch (e) {
-      if (e instanceof HttpError) throw e
       if (e.response) throw HttpError.CUSTOMIZE(e.response.status, e.response.data)
       throw e
     }
