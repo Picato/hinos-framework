30d432e9dc86c4d4f589c980cd0d49e8090d8b2c
thanhdt-viosoft <thanhdt@viosoft.com>
2018-02-23 10:08:11 +0700

    Update authoriz

--- src/service/Authoriz.ts
+++ src/service/Authoriz.ts
@@ -5,33 +5,22 @@ import HttpError from '../common/HttpError'
 
 export function authoriz(path: string, action: string) {
   return async ({ ctx, headers }: Context, next: Function) => {
+    const token = headers.token
+    if (!token) throw HttpError.AUTHEN()
     try {
       const res = await axios.head(`${AppConfig.services.oauth}/oauth/Authoriz`, {
-        params: {
-          path,
-          action
-        },
-        headers: {
-          token: headers.token
-        }
+        params: { path, action },
+        headers: { token }
       })
       ctx.state.auth = {
-        token: headers.token,
+        token,
         projectId: Mongo.uuid(res.headers.project_id),
         accountId: Mongo.uuid(res.headers.account_id)
       }
       await next()
     } catch (e) {
-      if (e instanceof HttpError) throw e
       if (e.response) throw HttpError.CUSTOMIZE(e.response.status, e.response.data)
       throw e
     }
-  }
-}
-
-export function suAuthoriz() {
-  return async ({ headers }: Context, next: Function) => {
-    if (headers.token && headers.token === AppConfig.app.suid) return await next()
-    throw HttpError.AUTHORIZ('This api need super admin permission')
   }
 }
