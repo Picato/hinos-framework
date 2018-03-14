dd6ab60fa8df9d2170888112cc13a080dd4bdf3e
thanhdt-viosoft <thanhdt@viosoft.com>
2018-03-14 17:07:41 +0700

    Update libs

--- src/env.production.ts
+++ src/env.production.ts
@@ -3,5 +3,4 @@ import { Logger } from "hinos-log/lib/logger";
 export default function env(Server) {
   Logger.pool().info('Production mode')
   if (AppConfig.encrypt.helmet) Server.use(require('hinos-helmet').helmet.default())
-  if (AppConfig.encrypt && AppConfig.encrypt.pwd) Server.use(require('hinos-requesthasher').hasher(AppConfig.encrypt.pwd, AppConfig.encrypt))
 }
